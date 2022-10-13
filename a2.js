1ad4aa7bb8a7e51b55fa0ecca4862aa005f33eda
HafsaZareen <94589319+HafsaZareen@users.noreply.github.com>
2022-10-13 23:42:44 +0530

    Add files via upload

---
+++ a2.js
@@ -0,0 +1,11 @@
+let btn=document.querySelector('button');
+// btn.addEventListener('click',showMsg);
+btn.addEventListener('click',showMsg);
+
+
+function showMsg()
+{
+    alert("Enjoy watching anime!!!!!!")
+}
+
+
